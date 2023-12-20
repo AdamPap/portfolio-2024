dad0dad49aab1395e8faedbdc10443e5a709f118
Adam Papamastorakis <adampapamast@hotmail.com>
2023-12-20 23:07:45 +0200

    add mouse follower component

---
+++ components/MouseFollower.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
+
+const MouseFollower = () => {
+  const mouseFollowerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    gsap.set(mouseFollowerRef.current, { xPercent: -50, yPercent: -50 });
+
+    document.addEventListener("mousemove", (e) => {
+      gsap.to(mouseFollowerRef.current, {
+        x: e.clientX,
+        y: e.clientY,
+        duration: 0.9,
+      });
+    });
+  }, []);
+
+  return (
+    <div
+      ref={mouseFollowerRef}
+      className="cursor-follower"
+    />
+  );
+};
+
+export default MouseFollower;
