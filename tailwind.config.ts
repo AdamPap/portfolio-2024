091f1337a4160eef8ca706d81a4d6f8164b3f0b6
Adam Papamastorakis <adampapamast@hotmail.com>
2023-12-20 23:07:14 +0200

    add custom theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        crimson: "#E32636",
+        lightGray: "#ECECEC",
+        darkGray: "#727272",
+        gridLines: "#DEDEDE",
+        gridLinesDark: "#303030",
+        bgDark: "#202020",
+        black: "#000000",
+        white: "#FFFFFF",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
