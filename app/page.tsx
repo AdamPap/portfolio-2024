cf4a079caa220e210b938158b3c77fb8d1dc5404
Adam Papamastorakis <adampapamast@hotmail.com>
2023-12-21 23:14:25 +0200

    add basic navbar

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,22 @@ export default function Home() {
         <div className="col-span-1 border-r-[1px] border-gridLines" />
         <div className="col-span-1 border-r-[1px] border-gridLines" />
       </div>
+
+      {/* Navbar */}
+      <nav className="grid grid-cols-4 py-6 text-[20px]">
+        <div className="col-span-1">Adam Pap.</div>
+        <div className="col-span-1">
+          Currently available for <br /> full time positions
+        </div>
+        <div className="col-span-1">hello@adampap.com</div>
+        <div className="col-span-1">
+          <div className="flex gap-10">
+            <a href="/projects">Projects</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
