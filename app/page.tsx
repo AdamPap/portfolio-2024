6f9a22515cf5edd872541a7f9323ba53695845c1
Adam Papamastorakis <adampapamast@hotmail.com>
2023-12-21 23:13:59 +0200

    add grid lines

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,15 @@ import MouseFollower from "../components/MouseFollower";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen">
-      <h1>HOME</h1>
+    <div className="w-full min-h-screen relative">
       <MouseFollower />
+      {/* Grid lines */}
+      <div className="grid grid-cols-4 h-screen w-full absolute -z-10 top-0 left-0 pointer-events-none">
+        <div className="col-span-1 border-r-[1px] border-l-[1px] border-gridLines" />
+        <div className="col-span-1 border-r-[1px] border-gridLines" />
+        <div className="col-span-1 border-r-[1px] border-gridLines" />
+        <div className="col-span-1 border-r-[1px] border-gridLines" />
+      </div>
     </div>
   );
 }
