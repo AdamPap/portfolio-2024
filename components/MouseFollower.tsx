4092e95a6097eb809280f2464101dbb934af96ac
Adam Papamastorakis <adampapamast@hotmail.com>
2023-12-24 21:27:10 +0200

    add inner cursor

--- components/MouseFollower.tsx
+++ components/MouseFollower.tsx
@@ -4,25 +4,39 @@ import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
 
 const MouseFollower = () => {
-  const mouseFollowerRef = useRef<HTMLDivElement>(null);
+  const cursorOuterRef = useRef<HTMLDivElement>(null);
+  const cursorInnerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    gsap.set(mouseFollowerRef.current, { xPercent: -50, yPercent: -50 });
+    gsap.set(cursorOuterRef.current, { xPercent: -50, yPercent: -50 });
+    gsap.set(cursorInnerRef.current, { xPercent: -50, yPercent: -50 });
 
     document.addEventListener("mousemove", (e) => {
-      gsap.to(mouseFollowerRef.current, {
+      gsap.to(cursorOuterRef.current, {
         x: e.clientX,
         y: e.clientY,
-        duration: 0.7,
+        duration: 0.4,
+      });
+
+      gsap.to(cursorInnerRef.current, {
+        x: e.clientX,
+        y: e.clientY,
+        duration: 0.1,
       });
     });
   }, []);
 
   return (
-    <div
-      ref={mouseFollowerRef}
-      className="cursor-follower"
-    />
+    <>
+      <div
+        ref={cursorOuterRef}
+        className="cursor-outer"
+      />
+      <div
+        ref={cursorInnerRef}
+        className="cursor-inner"
+      />
+    </>
   );
 };
 
