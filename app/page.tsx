2cc3db29d3bc00813650ab92b3abfd999a271a3c
Adam Papamastorakis <adampapamast@hotmail.com>
2023-12-30 19:23:07 +0200

    change grid lines from absolute to fixed

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <MouseFollower />
 
         {/* Grid lines */}
-        <div className="grid grid-cols-4 h-screen w-full absolute -z-[200] top-0 left-0 pointer-events-none">
+        <div className="grid grid-cols-4 min-h-screen w-full fixed px-6 -z-[200] top-0 left-0 pointer-events-none">
           <div className="col-span-1 border-r-[1px] border-l-[1px] border-gridLines" />
           <div className="col-span-1 border-r-[1px] border-gridLines" />
           <div className="col-span-1 border-r-[1px] border-gridLines" />
