04267aecb5229af9be985620a83ae9af56352f8e
Adam Papamastorakis <adampapamast@hotmail.com>
2024-01-03 23:39:47 +0200

    add project parallax image component

---
+++ components/ProjectImage.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React, { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+const ProjectImage = () => {
+  const imageContainerRef = React.useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    if (!imageContainerRef.current || !imageContainerRef.current.parentElement)
+      return;
+
+    const heightDiff =
+      imageContainerRef.current?.offsetHeight -
+      imageContainerRef.current?.parentElement?.offsetHeight;
+
+    gsap.fromTo(
+      imageContainerRef.current,
+      {
+        y: -heightDiff,
+      },
+      {
+        scrollTrigger: {
+          trigger: imageContainerRef.current.parentElement,
+          start: "top 80%",
+          end: "bottom center",
+          scrub: true,
+          markers: true,
+        },
+        ease: "none",
+        y: 0,
+      }
+    );
+  }, []);
+
+  return (
+    <div className="image-container">
+      <div
+        ref={imageContainerRef}
+        className="image-parallax"
+      ></div>
+    </div>
+  );
+};
+
+export default ProjectImage;
