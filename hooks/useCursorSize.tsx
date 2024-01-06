a0c11303364698701088139ba68d9b956feb8095
Adam Papamastorakis <adampapamast@hotmail.com>
2024-01-06 19:29:29 +0200

    add handlers in the hook

--- hooks/useCursorSize.tsx
+++ hooks/useCursorSize.tsx
@@ -2,11 +2,22 @@ import { CursorSizeContext } from "@/contexts/CursorSizeContext";
 import { useContext } from "react";
 
 export const useCursorSize = () => {
-  const context = useContext(CursorSizeContext);
+  const { cursorSize, setCursorSize } = useContext(CursorSizeContext);
 
-  if (!context) {
+  // add both handlers in the hook so that
+  // we don't have to add them to every component
+  // that uses the cursor
+  const handleMouseEnter = () => {
+    setCursorSize("large");
+  };
+
+  const handleMouseLeave = () => {
+    setCursorSize("normal");
+  };
+
+  if (!cursorSize || !setCursorSize) {
     throw new Error("useCursorSize must be used within a CursorSizeProvider");
   }
 
-  return context;
+  return { cursorSize, setCursorSize, handleMouseEnter, handleMouseLeave };
 };
