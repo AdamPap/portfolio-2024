f9de2ba2b8852a9c4a8cfa3bb9f52e7162ed6f40
Adam Papamastorakis <adampapamast@hotmail.com>
2024-01-07 22:59:44 +0200

    change to next Image component

--- components/ProjectImageParallax.tsx
+++ components/ProjectImageParallax.tsx
@@ -3,12 +3,14 @@
 import React, { useEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Image, { StaticImageData } from "next/image";
 
 interface Props {
-  projectIndex: number;
+  image: StaticImageData;
+  index: number;
 }
 
-const ProjectImageParallax: React.FC<Props> = ({ projectIndex }) => {
+const ProjectImageParallax: React.FC<Props> = ({ image, index }) => {
   const imageContainerRef = React.useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -41,11 +43,37 @@ const ProjectImageParallax: React.FC<Props> = ({ projectIndex }) => {
   }, []);
 
   return (
-    <div className="image-container">
+    // The cropped container with overflow hidden.
+    <div className={`image-container`}>
+      {/* 
+          The big image container which will 
+          allow the parallax to work. This would be
+          the image if next image component didn't 
+          need this fix.
+      */}
       <div
+        className="relative"
         ref={imageContainerRef}
-        className={`image-parallax bg-[url('/images/project-${projectIndex}.jpg')]`}
-      ></div>
+      >
+        {/* 
+            This is to allow next image have fill
+            property to simulate bg-image: cover.
+            The image takes the size of the parent. 
+            Next Image component requires relative 
+            parent.
+        */}
+        <div className="next-image-container">
+          <Image
+            alt="Project Image"
+            src={image}
+            fill
+            quality={100}
+            style={{
+              objectFit: "cover",
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
